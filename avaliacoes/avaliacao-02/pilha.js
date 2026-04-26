c4c5cdead3b5fb473dd9da23d3b32be5fd0bded4
aldamir007 <160159143+aldamir007@users.noreply.github.com>
2026-04-26 16:38:02 -0300

    Update pilha.js

--- avaliacoes/avaliacao-02/pilha.js
+++ avaliacoes/avaliacao-02/pilha.js
@@ -1,1 +1,47 @@
+class Pilha {
+    constructor() {
+        this.itens = [];
+    }
 
+    empilhar(elemento){
+        this.itens.push(elemento);
+        console.log(elemento + " empilhado com sucesso!");
+    }
+
+    desempilhar(){
+        if(this.itens.length === 0){
+            console.log("Pilha vazia");
+            return;
+        }
+
+        let removido = this.itens.pop();
+        console.log(removido + " foi desempilhado.");
+    }
+
+    topo(){
+        if(this.itens.length === 0){
+            console.log("Pilha vazia");
+            return;
+        }
+
+        console.log("Topo da pilha:", this.itens[this.itens.length-1]);
+    }
+
+    mostrar(){
+        console.log("Elementos da pilha:");
+        for(let i = this.itens.length-1; i >=0; i--){
+            console.log(this.itens[i]);
+        }
+    }
+}
+
+const pilha = new Pilha();
+
+pilha.empilhar("HTML");
+pilha.empilhar("CSS");
+pilha.empilhar("JavaScript");
+
+pilha.mostrar();
+pilha.topo();
+pilha.desempilhar();
+pilha.mostrar();
